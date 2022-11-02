5ad0fe4fafa9e4365e7f609cccdd7d9c78df98a4
Sanjana Kumari <58020266+sanju2900@users.noreply.github.com>
2022-11-02 22:25:59 +0530

    Add files via upload

---
+++ 28-10-2022/Movie_info_ass3/Movie_info_ass3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Data.SqlClient;
+
+namespace Movie_info_ass3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string ConnectionString = "Data Source=INLPF1WRZPF;Initial Catalog=sanjana;trusted_connection=true";
+                SqlConnection sqlCon = new SqlConnection(ConnectionString);
+                sqlCon.Open();
+
+                Console.WriteLine("Enter The Choice");
+                string choice = Console.ReadLine();
+
+                string query = @"select Horror_movies,Kids_movies from Movies ";
+                SqlCommand cmd = new SqlCommand(query, sqlCon);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                int i = 0;
+                while(reader.Read())
+                {
+                    if (i == 5) break;
+
+                    switch (choice)
+                    {
+                        case "horror":
+                            {
+
+                                Console.Write(reader[0]);
+
+                            }
+                            break;
+                        case "kids":
+                            {
+
+                                Console.WriteLine(reader[1]);
+                            }
+                            break;
+                    }
+                    i++;
+                    Console.WriteLine();
+                }
+                reader.Close();
+                sqlCon.Close();
+
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Sql problem");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("C# problem");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
